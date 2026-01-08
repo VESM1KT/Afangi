d6d9e53b925d4d3d73a7a3b9b705363cdfcf0800
Gunnar Þórunnarson <gus@tskoli.is>
2026-01-08 00:31:11 +0000

    Improve comments for clarity in led.py

--- Synidaemi/led.py
+++ Synidaemi/led.py
@@ -4,17 +4,17 @@
 from machine import Pin    # Notum Pin úr machine kóðasafninu til að skilgreina pinnana á ESP. 
 from time import sleep_ms  # Notum svo sleep_ms til að láta forritið bíða
 
-# Búum til breytuna led og tengjum hana við pinna 4 á ESP. 
-led = Pin(4, Pin.OUT)   # Segjum að pinni 4 sé úttakspinni.
+# Búum til breytuna led og tengjum hana við pinna 4 á ESP sem er úttakspinni. 
+led = Pin(4, Pin.OUT)   
 
-led_state = False  # Our boolean variable
+led_state = False  # boolean True eða False
 
 while True:
-    # Set the LED to the current boolean state (False=0, True=1)
-    led.value(led_state)
+    # Stillum Led kveikja/slökkva eftir því hvað boolean breytan segir (False=0, True=1)
+    led.value(led_state) 
     
-    # Flip the boolean
+    # Snúum boolean gildinu við (True verður False, eða False verður True)
     led_state = not led_state
     
-    # Wait 500 milliseconds
+    # Bíðum 500 milliseconds
     sleep_ms(500)
